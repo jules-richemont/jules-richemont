e516d7cab8379ac1ae868d6dd74fdd8953a18e38
Jules de Richemont <99812886+jules-richemont@users.noreply.github.com>
2022-05-24 17:31:21 +0200

    Delete entreeSortieLC.h

--- entreeSortieLC.h
+++
@@ -1,9 +0,0 @@
-#ifndef ENTREESORTIELC_H
-#define ENTREESORTIELC_H
-#include "biblioLC.h"
-
-
-Biblio* charger_n_entrees(char* nomfic, int n);
-void enregistrer_biblio(Biblio b, char nomfic);
-
-#endif
